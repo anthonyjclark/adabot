d1e733f3c73fcf0e0152032f801b2af22a06fb54
Anthony J. Clark <ajc@>
2017-05-31 17:26:53 -0500

    Added apt-get update to CI.

--- .travis.sh
+++ .travis.sh
@@ -6,6 +6,7 @@ set -e
 
 # ---------------------------------------------------------
 printf "\nInstall testing tools.\n"
+apt-get update
 apt-get install wget
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | apt-key add -
