9f28dedfbef25a579a97188eae21814137ce34b2
Anthony J. Clark <anthonyjclark@gmail.com>
2017-06-12 12:39:35 -0500

    The IMU sensor has been added.

--- adabot_control/scripts/test_subscriber.py
+++ adabot_control/scripts/test_subscriber.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 """
+For detecting slippage, we'll need to do the following:
+
+1. Subscribe to the /adabot/joint_states topic.
+2. Convert wheel angular velocities into linear velocity
+    a. This requies us to know the radius of the wheels (parameter server?)
+    b. We'll also need to take into acount the weg extension length
+3. Subscribe to the
+
+
 > rosmsg show sensor_msgs/JointState
 std_msgs/Header header
   uint32 seq
@@ -17,13 +26,14 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
 
 def callback(data):
-    wheel_indices = [i for i, s in enumerate(data.name) if '_wheel_join' in s]
-    wheel_names = [data.name[i] for i in wheel_indices]
-    wheel_positions = [data.position[i] for i in wheel_indices]
-    wheel_velocities = [data.velocity[i] for i in wheel_indices]
-    wheel_efforts = [data.effort[i] for i in wheel_indices]
-    for n, p, v, e in zip(wheel_names, wheel_positions, wheel_velocities, wheel_efforts):
-        print n, ":", p, v, e
+    # wheel_indices = [i for i, s in enumerate(data.name) if '_wheel_join' in s]
+    # wheel_names = [data.name[i] for i in wheel_indices]
+    # wheel_positions = [data.position[i] for i in wheel_indices]
+    # wheel_velocities = [data.velocity[i] for i in wheel_indices]
+    # wheel_efforts = [data.effort[i] for i in wheel_indices]
+    # for n, p, v, e in zip(wheel_names, wheel_positions, wheel_velocities, wheel_efforts):
+    #     print n, ":", p, v, e
+
 
 def listener():
     rospy.init_node('listener', anonymous=True)
